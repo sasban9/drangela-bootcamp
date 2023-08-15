dd495513607f0ffa071aa7d61165767340fa5e84
Saswata Banerjee <azure.saswata@gmail.com>
2023-08-15 21:49:13 +0530

    end of lecture 347

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -37,37 +39,87 @@ mongoose.connect(process.env.MONGODB_CONNECT + "userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Google profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+      userProfileURL: process.env.USER_PROFILE_URL,
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/logout", (req, res) => {
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
 app.get("/secrets", (req, res) => {
-  if (req.isAuthenticated()) res.render("secrets"); // redirect to secrets if user is already authenticated
+  if (req.isAuthenticated()) res.render("secrets");
+  // redirect to secrets if user is already authenticated
   else res.redirect("/login");
 });
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
 
 app
   .route("/login")
   .get((req, res) => {
-    if (req.isAuthenticated()) res.redirect("/secrets"); // redirect to secrets if user is already authenticated
+    if (req.isAuthenticated()) res.redirect("/secrets");
+    // redirect to secrets if user is already authenticated
     else res.render("login");
   })
   .post((req, res) => {
@@ -90,7 +142,8 @@ app
 app
   .route("/register")
   .get((req, res) => {
-    if (req.isAuthenticated()) res.redirect("/secrets"); // redirect to secrets if user is already authenticated
+    if (req.isAuthenticated()) res.redirect("/secrets");
+    // redirect to secrets if user is already authenticated
     else res.render("register");
   })
 

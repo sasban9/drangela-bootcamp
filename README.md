fcff429334e4b299c2cf7c5b2fb35a08bde25906
Saswata Banerjee <azure.saswata@gmail.com>
2023-08-15 10:31:56 +0530

    end of lecture 337

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+Section 39: Commands for terminal
+
+Lecture 337
+
+`npm init -y`
+
+`npm i express ejs body-parser`
+
+`nodemon app.js`
+
 ## Every branch is a section ;-)
 
 
---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Server Running at Port 3000");
+});
